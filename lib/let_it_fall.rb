61c6c2375b7d3c2763936b4a8cb5b13c8b7ff82e
kyoendo <postagie@gmail.com>
2014-05-04 22:33:25 +0900

    refactoring Render class

--- lib/let_it_fall.rb
+++ lib/let_it_fall.rb
@@ -11,16 +11,19 @@ module LetItFall
     def initialize(mark, screen, colorize:false, interval:0.1)
       @y, @x = screen
       @colorize = colorize
-      marks =
-        if Array===mark
-          mark.map { |m| m.to_i(16) }
-        else
-          CODESET[mark.intern] || CODESET[:snow]
-        end
-      @marks = Array(marks).map { |code| code.chr("UTF-8") }.cycle
+      @marks = build_marks(mark)
       @interval = interval
       @screen = {}
       $stdout.sync = true
+    end
+
+    def build_marks(mark)
+      marks =
+        case mark
+        when Array then mark.map { |m| m.to_i(16) }
+        else CODESET[mark.intern] || CODESET[:snow]
+        end
+      Array(marks).map { |code| code.chr("UTF-8") }.cycle
     end
 
     def run
@@ -30,7 +33,7 @@ module LetItFall
           print "\e[?25h\e[0;0H" # show cursor and set 0,0 pos
           exit(0)
         end
-        print_marks
+        print_marks(rand(@x), 0, @marks.dup)
         sleep @interval
       end
     end
@@ -40,9 +43,9 @@ module LetItFall
       print "\e[?25l\e[2J"
     end
 
-    def print_marks
-      @screen[rand @x] = [0, @marks.dup]
-      @screen.each do |x, (y,mark)|
+    def print_marks(pos_x, pos_y, mark)
+      @screen[pos_x] = [pos_y, mark]
+      @screen.each do |x, (y, mark)|
         @screen[x][0] += 1 if @screen[x][0] <= @y*0.9
         clear_prev_mark(x, y)
         color = @colorize ? [*31..37].sample : 37
